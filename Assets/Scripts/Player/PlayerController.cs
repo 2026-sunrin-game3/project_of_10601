0eac2d3175c37185eaeac94b7267a3fc0d650668
TKvirous <roykim2703@gmail.com>
2026-07-09 17:41:20 +0900

    player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInput input;
+    public PlayerMovement movement;
+
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        movement.move(input.axis);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public float moaveSpeed = 3f, jumpPower = 12f;
+    [SerializeField] LayerMask groundMask_;
+    [SerializeField] float groundDist_ = 0.5f;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void move(Vector2 axis)
+    {
+        transform.Translate(axis.normalized * moaveSpeed * Time.deltaTime);
+    }
+
+    public void Setvelocity(Vector2 dir)
+    {
+        rigid.linearVelocity = dir;
+    }
+
+    public bool OnGround()
+    {
+        Vector2 center = transform.position + Vector3.down * groundDist_ * 0.5f;
+        Vector2 size = new Vector3(0.3f, groundDist_);
+        Collider2D[] cast = Physics2D.OverlapBoxAll(center, size, 0f, groundMask_);
+
+        return cast.Length > 0;
+    }
+    public void Jump()
+    {
+        if(OnGround())
+        {
+            Setvelocity(Vector2.up * jumpPower);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position + Vector3.down * groundDist_ * 0.5f, new Vector3(0.3f, groundDist_));
+    }
+}
